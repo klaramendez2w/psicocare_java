444d3704d1a965342ecdac226913bdfd7ea212ba
Rebel1898 <Sergio.Llamas@protonmail.ch>
2018-11-29 15:49:15 +0100

    Resultados del test se muestran dinamicamente

--- PsicoCare/src/main/java/com/psicocare/controllers/TaketestServlet.java
+++ PsicoCare/src/main/java/com/psicocare/controllers/TaketestServlet.java
@@ -25,7 +25,8 @@ public class TaketestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		doGet(request,response);
 	}
 
 	/**
