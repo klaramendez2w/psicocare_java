084ebcb11cd9a8759a8e847bf67a8b2f9caea268
Rebel1898 <Sergio.llamas@protonmail.ch>
2018-11-29 12:58:54 +0100

    modelosyDAO

---
+++ PsicoCare/src/main/java/com/psicocare/models/Activity.java
@@ -0,0 +1,33 @@
+package com.psicocare.models;
+
+public class Activity {
+	private int aid;
+	private String statement;
+	private String value;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getStatement() {
+		return statement;
+	}
+	public void setStatement(String statement) {
+		this.statement = statement;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Activity(int aid, String statement, String value) {
+		super();
+		this.aid = aid;
+		this.statement = statement;
+		this.value = value;
+	}
+
+
+}
