49934586bf15950479769e9261bf9cfdac9f99d1
belen1990 <belen_lau@hotmail.com>
2018-11-30 14:21:23 +0100

    corre

--- PsicoCare/src/main/java/com/psicocare/controllers/MenuServlet.java
+++ PsicoCare/src/main/java/com/psicocare/controllers/MenuServlet.java
@@ -1,38 +1,50 @@
 package com.psicocare.controllers;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MenuServlet
- */
+import com.psicocare.DAO.ActivityDAO;
+import com.psicocare.models.Activity;
+
+
 public class MenuServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MenuServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		
+		request.getRequestDispatcher("/menu.jsp").forward(request, response);		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		String aidrec = request.getParameter("aid");
+
+		int aidInt = Integer.parseInt(aidrec);
+
+		Activity myActivity = null;
+		try {
+			myActivity = ActivityDAO.getInstance().getActivityByAid(aidInt);
+			
+			System.out.println(aidInt);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		request.setAttribute("miActivity", myActivity);
+		
+		
+		
 		doGet(request, response);
 	}
 
